84d8f515e27ae28664662175328a66b6b2b9f541
SaraAliMohammed <Sara@gmail.com>
2024-03-19 15:02:21 +0200

    Add register template post and validate code with error messages

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 starts a Flask web application
 """
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '972c87379d16a8a27665666e1cf17a35c44d39d4feddceda9d600c4d8b304436'
 
 lessons = [{
     'title': 'Request Library Course',
@@ -88,6 +89,21 @@ def home():
 @app.route("/about")
 def about():
 	return render_template('about.html', title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f"Account created successfully for {form.username.data}", "success")
+		return redirect(url_for("home"))
+	return render_template('register.html', title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title="Login", form=form)
 
 
 if __name__ == "__main__":
