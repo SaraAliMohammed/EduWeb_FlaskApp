1d4ee4071702004eaa03879b2ea903263e8131cd
SaraAliMohammed <Sara@gmail.com>
2024-03-20 14:42:10 +0200

    add SQLALchemy and make User, Lesson, Course Models

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlachemy import SQLAlchemy
+from datetime import datetime
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '972c87379d16a8a27665666e1cf17a35c44d39d4feddceda9d600c4d8b304436'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eduweb.db'
+db = SQLAlchemy(app)
 lessons = [{
     'title': 'Request Library Course',
     'course': 'Python',
@@ -113,6 +116,45 @@ def login():
 		else:
 			flash("Login Unsuccessful. Please check credentials", "danger")
 	return render_template('login.html', title="Login", form=form)
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	fname = db.Column(db.String(25), nullable=False)
+	lname = db.Column(db.String(25), nullable=False)
+	username = db.Column(db.String(25), nullable=False, unique=True)
+	email = db.Column(db.String(125), nullable=False, unique=True)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	lessons = db.relationship("Lesson", backref='author', lazy=True)
+
+	def __repr__(self):
+		return f"USER('{self.fname}', '{self.lname}', '{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Lesson(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	thumbnail = db.Column(db.String(20), nullable=False, default='default_thumbnail.jpg')
+	slug = db.Column(db.String(32), nullable=False)
+	user_id = db.Column(db.Foreignkey, 'user.id', nullable=False)
+	course_id = db.Column(db.Foreignkey, 'course.id', nullable=False)
+
+	def __repr__(self):
+		return f"LESSON('{self.title}', '{self.date_posted}')"
+
+
+class Course(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(50), nullable=False, unique=True)
+	description = db.Column(db.String(150), nullable=False)
+	icon = db.Column(db.String(20), nullable=False, default='default_icon.jpg')
+	lessons = db.relationship("Lesson", backref='course_name', lazy=True)
+
+	def __repr__(self):
+		return f"COURSE('{self.title}')"
 
 
 if __name__ == "__main__":
